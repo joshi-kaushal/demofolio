e47a465f10cc93dc5746558f01ca5fb838e09b81
Kaushal Joshi <7joshikaushal@gmail.com>
2021-09-05 17:01:28 +0530

    designed header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+import { DarkmodeSwitch } from "reacthalfmoon";
+import Header from "./Components/Header/Header";
 
 function App() {
+  const [darkmode, setDarkmode] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header darkmode={darkmode} setDarkmode={setDarkmode} />
     </div>
   );
 }
 
 export default App;
+
+/* Components: 
+  Header
+  Hero
+  Tech Stack
+  Projects
+  Experience
+  Education
+  Contact
+*/
